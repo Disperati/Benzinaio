bddc2f4e61de128075d5a6454e43eddbdddce7c8
Piercostanzo Gennaro <piercostanzo.gennaro@gmail.com>
2020-03-14 12:07:00 +0100

    Modificata Camera 1

--- Benzinaio/Camera.cs
+++ Benzinaio/Camera.cs
@@ -9,17 +9,23 @@ namespace Benzinaio
     public class Camera
     {
 
-        Tipo[] tipo = { new Tipo("Singola", "S"),new Tipo("Doppia", "D"), new Tipo("Suite Reale", "X"),
+       static Tipo[] tipo = { new Tipo("Singola", "S"),new Tipo("Doppia", "D"), new Tipo("Suite Reale", "X"),
                         new Tipo("Bungolow", "B"), new Tipo("Tugurio", "U") };
        
         private string idCamera;  //"Es S-01, D-01, U-10 Prima Lettera indica il tipo - Due Cifre indicano il numero progressivo della camera."
         private int nPosti;
         private string[] optional;
-        public Camera(String tipoCamera, int nPosti, params string[] opt)
+        private string tipoCamera;
+        private string etichetta;
+        
+        public Camera(String TipoCamera, int NPosti, params string[] opt)
         {
-            NPosti = nPosti;
+            this.NPosti = nPosti;
             Optional = opt;
-            assegnaID(tipoCamera);   
+            IdCamera = TipoCamera;
+            this.TipoCamera = TipoCamera;
+            Etichetta = TipoCamera;
+
             
         }
 
@@ -32,7 +38,7 @@ namespace Benzinaio
         public string[] Optional { get => optional; set
             {
                 if (value.Length == 0)
-                    optional[0] = " Nessun optional";
+                    optional[0] = "Nessun optional";
                 else
                 {
                     optional = new String[value.Length];
@@ -42,20 +48,36 @@ namespace Benzinaio
             }
         }
 
-        public void assegnaID(String nomeCamera)
-        {
-            foreach (Tipo t in tipo)
+        public string TipoCamera { get => tipoCamera; set => tipoCamera = value; }
+        public string IdCamera { get => idCamera; set
             {
-                if (t.nome == nomeCamera)
+                foreach (Tipo t in tipo)
                 {
-                    idCamera = t.numeroCamera > 10 ? $"{t.identificativo}-{t.numeroCamera++}" :
-                                                     $"{t.identificativo}-0{t.numeroCamera++}";
-                    break;
+                    if (t.nome == value)
+                    {
+
+                        idCamera = t.numeroCamera > 10 ? $"{t.identificativo}-{t.numeroCamera++}" :
+                                                         $"{t.identificativo}-0{t.numeroCamera++}";
+                        break;
+                    }
+
+                    throw new ArgumentException("Camera non Trovata");
                 }
 
-                throw new ArgumentException("Non Esiste il tipo di Camera");
             }
+        }
 
+        public string Etichetta { get => etichetta; set
+            {
+                foreach (Tipo t in tipo)
+                    if (t.nome == value)
+                        etichetta = t.identificativo;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Stampare Attributi ";
         }
 
         public class Tipo
